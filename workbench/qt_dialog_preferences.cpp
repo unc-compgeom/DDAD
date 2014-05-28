79cc2fbadb49c5a570882adecbf7c0e6cc5e87ef
freemancw <freeman@cs.unc.edu>
2014-05-28 09:18:53 -0400

    removed license

--- workbench/qt_dialog_preferences.cpp
+++ workbench/qt_dialog_preferences.cpp
@@ -1,18 +1,3 @@
-/*
- * This file is part of RationalCAD.
- *
- * RationalCAD is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * RationalCAD is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details. You should have received a copy of the GNU General Public
- * License along with RationalCAD. If not, see <http://www.gnu.org/licenses/>.
- */
-
 /*!
  * @author Clinton Freeman <freeman@cs.unc.edu>
  * @date 01/29/2013
@@ -21,7 +6,7 @@
 // Qt
 #include <QColorDialog>
 
-// RationalCAD
+// Workbench
 #include "common.h"
 #include "qt_dialog_preferences.h"
 #include "ui_dialog_preferences.h"
