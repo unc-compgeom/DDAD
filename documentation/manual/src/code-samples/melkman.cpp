167897c1545ef6687045185164c16c65c35c1876
freemancw <freeman@cs.unc.edu>
2014-07-13 13:03:25 -0400

    updated melkman code snippet

--- documentation/manual/src/code-samples/melkman.cpp
+++ documentation/manual/src/code-samples/melkman.cpp
@@ -1,38 +1,25 @@
-Polygon_2rDq Melkman(const PolyChain_2r& P, Visual::IGeometryObserver* observer){
-    Polygon_2rDq D;
-    D.AddObserver(observer);
-    std::list<PolyChainVertex_2r> vertices = P.vertices();
-    std::list<PolyChainVertex_2r>::iterator it = vertices.begin();
-    PolyChainVertex_2r v0 = *it; it++;
-    PolyChainVertex_2r v1 = *it; it++;
-    D.PushFront(v1);
-    D.PushFront(v0);
-    D.PushFront(v1);
+Polygon_2r Melkman(const Polyline_2r& P, Visual::IGeometryObserver* observer) {
+    Polygon_2r hull;
+    hull.AddObserver(observer);
 
-    for(std::list<PolyChainVertex_2r>::iterator ii = it; ii != vertices.end(); ii ++){
-        PolyChainVertex_2r pi = *ii;
-        Line_2r topEdge = Line_2r(D[0].vertex_sptr(), D[1].vertex_sptr());
-        Ray_2r topRay = Ray_2r(D[1].vertex_sptr(), topEdge.V());
-        Line_2r botEdge = Line_2r(D[D.NumVertices()-2].vertex_sptr(), D[D.NumVertices()-1].vertex_sptr());
-        Ray_2r botRay = Ray_2r(D[D.NumVertices()-1].vertex_sptr(), botEdge.V());
+    // initialize hull
+    hull.push_back(P[0]);
+    hull.push_back(P[1]);
+    hull.push_back(P[0]);
 
-        if((Predicate::AIsRightOfB(pi.vertex(), topEdge) || Predicate::AIsAheadOfB(pi.vertex(), topRay)) ||
-        (Predicate::AIsRightOfB(pi.vertex(), botEdge) || Predicate::AIsAheadOfB(pi.vertex(), botRay))){
-            while(Predicate::AIsRightOfB(pi.vertex(), topEdge) || Predicate::AIsAheadOfB(pi.vertex(), topRay)){
-                // Pop top
-                D.PopFront();
-                topEdge = Line_2r(D[0].vertex_sptr(), D[1].vertex_sptr());
-                topRay = Ray_2r(D[1].vertex_sptr(), topEdge.V());
+    for (size_t i = 2; i < P.size(); ++i) {
+        if (!RIsLeftOrInsidePQ(*hull.back(1), *hull.back(0), *P[i]) ||
+            !RIsLeftOrInsidePQ(*P[i], *hull.front(1), *hull.front(0))) {
+            while (!RIsLeftOrInsidePQ(*hull.back(1), *hull.back(0), *P[i])) {
+                hull.pop_back();
             }
-            while(Predicate::AIsRightOfB(pi.vertex(), botEdge) || Predicate::AIsAheadOfB(pi.vertex(), botRay)){
-                // Pop bot
-                D.PopBack();
-                botEdge = Line_2r(D[D.NumVertices()-2].vertex_sptr(), D[D.NumVertices()-1].vertex_sptr());
-                botRay = Ray_2r(D[D.NumVertices()-1].vertex_sptr(), botEdge.V());
+            while (!RIsLeftOrInsidePQ(*P[i], *hull.front(0), *hull.front(1))) {
+                hull.pop_front();
             }
-            D.PushFront(pi);
-            D.PushBack(pi);
+            hull.push_back(P[i]);
+            hull.push_front(P[i]);
         }
     }
-    return D;
+
+    return hull;
 }
