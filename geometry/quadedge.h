0529600a5ba91b9fb44641209077c42ae0d9bc93
Clinton Freeman <freeman@cs.unc.edu>
2015-07-19 23:40:29 -0400

    Update quadedge.h

--- geometry/quadedge.h
+++ geometry/quadedge.h
@@ -1,16 +1,5 @@
 /*
- * This file is part of DDAD.
- *
- * DDAD is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
- *
- * DDAD is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details. You should have received a copy of the GNU General Public
- * License along with DDAD. If not, see <http://www.gnu.org/licenses/>.
+ * Code adapted from http://www.cs.cmu.edu/afs/andrew/scs/cs/15-463/2001/pub/src/a2/quadedge.html 
  */
 
 #ifndef GE_QUADEDGE_H
